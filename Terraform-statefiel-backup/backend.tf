99bc5e71fb0950a075cee93ed473b7b9e8fc502a
Manoj G K <manojgk0210@gmail.com>
2026-02-02 15:22:53 +0530

    Add Terraform backend configuration for S3

---
+++ Terraform-statefiel-backup/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "manoj-terraform-state-bucket-002"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
