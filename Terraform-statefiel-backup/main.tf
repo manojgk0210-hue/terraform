b893799bb4030964c5666ddec75eb6a78fd65e13
Manoj G K <manojgk0210@gmail.com>
2026-02-02 15:22:20 +0530

    Create main.tf

---
+++ Terraform-statefiel-backup/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "manoj-terraform-state-bucket-002"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
